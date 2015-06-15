650483670a3d13e3dd201c00615b1311c30c9867
jgabry <jgabry@gmail.com>
2015-06-15 13:34:21 -0600

    cleaning

--- R/gpdfit.R
+++ R/gpdfit.R
@@ -16,9 +16,9 @@ gpdfit <- function(x) {
   m <- 80 + floor(sqrt(n))  # note: original paper used m <- 20+floor(sqrt(n))
   b <- 1/x[n] + (1 - sqrt(m/seq_min_half(m)))/prior/x[floor(n/4 + 0.5)]
   L <- vapply1m(m, function(i) n * lx(b[i], x))
-  w <- vapply1m(m, function(i) 1/sum(exp(L - L[i])))
+  w <- 1 / vapply1m(m, function(i) sum(exp(L - L[i])))
   b <- sum(b * w)
   k <- mean.default(log(1 - b * x))
   sigma <- -k/b
-  list(k = k, sigma = sigma)
+  nlist(k, sigma)
 }
