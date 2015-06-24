8bb3fe852f1e5333e0f2fd5ac33ca94c07558315
jgabry <jgabry@gmail.com>
2015-06-23 20:34:58 -0400

    R >= 2.10 dependence
    [ci skip]

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -16,7 +16,7 @@ seed <- loo_and_waic_dat$seed
 x <- loo_and_waic_dat$x
 ans <- loo_and_waic_dat$ans
 set.seed(seed)
-loo <- loo_and_waic(x)
+loo <- loo_and_waic(x, cores = 1)
 loo_diff <- unlist(loo_and_waic_diff(loo, loo))
 test_that("loo_and_waic returns correct result", {
   expect_identical(loo, ans)
