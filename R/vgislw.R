01cbc7deaf93981840e9d2eed81dfeb38928d757
jgabry <jgabry@gmail.com>
2015-06-25 15:32:44 -0400

    update vgislw for more numerical stability
    pmax(x2, max(x2) - thresh)
    
    also convert to matrix if vector
    
    k <- min(fit$k, kmax)

--- R/vgislw.R
+++ R/vgislw.R
@@ -1,22 +1,27 @@
 #' VGIS: Very good importance sampling
 #'
 #' @export
-#' @param lw a matrix of log weights, For for computing LOO \code{lw = -log_lik}
-#'   (see \code{\link{extract_log_lik}}) and is an \eqn{S} by \eqn{N} matrix
-#'   where \eqn{S} is the number of simulations and \eqn{N} is the number of
-#'   data points.
-#' @param wcp the percentage of importance weights to use for the generalized
-#'   Pareto fit. The \code{wcp}\% largest weights are used as the sample from
-#'   which to estimate the parameters of the generalized Pareto distribution.
+#' @param lw a matrix or vector of log weights. For for computing LOO \code{lw =
+#'   -log_lik} (see \code{\link{extract_log_lik}}) and is an \eqn{S} by \eqn{N}
+#'   matrix where \eqn{S} is the number of simulations and \eqn{N} is the number
+#'   of data points. (If \code{lw} is a vector it will be coerced to a
+#'   one-column matrix.)
+#' @param wcp the proportion of importance weights to use for the generalized
+#'   Pareto fit. The \code{100*wcp}\% largest weights are used as the sample
+#'   from which to estimate the parameters of the generalized Pareto
+#'   distribution.
+#' @param thresh when used for the generalized pareto fit, the largest
+#'   \code{100*wcp}\% of the importance weights are modified according to
+#'   \code{pmax(x, max(x) - thresh)} for numerical stability.
+#' @param kmax maximum allowed value for the generalized Pareto shape parameter
+#'   \eqn{k}.
 #' @param wtrunc for truncating very large weights to \eqn{S}^\code{wtrunc}. Set
 #'   to zero for no truncation.
-#' @param fix_value if the largest weight in any column of \code{lw} is greater
-#'   than the second largest by at least \code{fix_value}, the second largest
-#'   will be set equal to the largest. Set to zero to skip this step.
 #' @param cores the number of cores to use for parallelization.
 #'
-#' @return A named with list with components \code{lw} (modified log weights)
-#' and \code{k} (tail indices for the Pareto \eqn{k} estimates).
+#' @return A named with list with components \code{lw_smooth} (modified log
+#'   weights) and \code{pareto_k} (estimated generalized Pareto shape parameters
+#'   \eqn{k}).
 #'
 #'
 #' @details See the 'VGIS-LOO' section in \code{\link{loo-package}}.
@@ -31,30 +36,33 @@
 #' @importFrom matrixStats logSumExp
 #' @importFrom parallel mclapply makePSOCKcluster stopCluster parLapply
 #'
-vgislw <- function(lw, wcp = 20, wtrunc = 3/4, fix_value = 100,
+vgislw <- function(lw, wcp = 0.2, thresh = 100, kmax = 2, wtrunc = 3/4,
                    cores = parallel::detectCores()) {
   .vgis <- function(i) {
     x <- lw[, i]
-    # divide log weights into body and right tail
     S <- length(x)
-    cutoff <- quantile(x, 1 - wcp / 100, names = FALSE)
+    # split into body and right tail
+    cutoff <- quantile(x, 1 - wcp, names = FALSE)
     x_cut <- x > cutoff
-    x1 <- x[!x_cut]
-    x2 <- x[x_cut]
-    n2 <- length(x2)
+    xbody <- x[!x_cut]
+    xtail <- x[x_cut]
+    tail_len <- length(xtail)
     # store order of tail samples
-    x2_order <- order(x2)
+    tail_ord <- order(xtail)
     # fit generalized Pareto distribution to the right tail samples
+    xtail <- pmax(xtail, max(xtail) - thresh)
     exp_cutoff <- exp(cutoff)
-    fit <- gpdfit(exp(x2) - exp_cutoff)
+    fit <- gpdfit(exp(xtail) - exp_cutoff)
+    k <- min(fit$k, kmax)
     # compute order statistics for the fit
-    qq <- qgpd(seq_min_half(n2) / n2, xi = fit$k, beta = fit$sigma) + exp_cutoff
+    qq <- qgpd(seq_min_half(tail_len)/tail_len, xi = k, beta = fit$sigma)
+    qq <- qq + exp_cutoff
     # remap back to the original order
-    slq <- rep.int(0, n2)
-    slq[x2_order] <- log(qq)
-    # join body and GPD smoothed tail
+    slq <- rep.int(0, tail_len)
+    slq[tail_ord] <- log(qq)
+    # join body and gPd smoothed tail
     qx <- x
-    qx[!x_cut] <- x1
+    qx[!x_cut] <- xbody
     qx[x_cut] <- slq
     if (wtrunc > 0) {
       # truncate
@@ -65,25 +73,23 @@ vgislw <- function(lw, wcp = 20, wtrunc = 3/4, fix_value = 100,
     # renormalize weights
     lwx <- qx - logSumExp(qx)
     # return log weights and tail index k
-    list(lwx, fit$k)
+    list(lwx, k)
   }
 
-  if (fix_value > 0) {
-    lw <- fix_large_diffs(lw, fix_value = fix_value)
-  }
+  if (!is.matrix(lw))
+    lw <- as.matrix(lw)
   K <- ncol(lw)
   K2 <- 2 * K
   if (.Platform$OS.type != "windows") {
-    out <- mclapply(X = 1:K, FUN = .vgis, mc.cores = cores)
+    vgis_out <- mclapply(X = 1:K, FUN = .vgis, mc.cores = cores)
   } else {
     cl <- makePSOCKcluster(cores)
     on.exit(stopCluster(cl))
-    out <- parLapply(cl, X = 1:K, fun = .vgis)
+    vgis_out <- parLapply(cl, X = 1:K, fun = .vgis)
   }
-  ux <- unlist(out, recursive = FALSE, use.names = FALSE)
-  lw <- do.call(cbind, ux[seq(1, K2, 2)])
-  kss <- do.call(c, ux[seq(2, K2, 2)])
-
-  # return log weights and tail indices k
-  nlist(lw, k = kss)
+  # extract and return modified log weights and gPd shape param k estimates
+  ux <- unlist(vgis_out, recursive = FALSE, use.names = FALSE)
+  lw_smooth <- cbind_list(ux[seq(1, K2, 2)])
+  pareto_k <- do.call(c, ux[seq(2, K2, 2)])
+  nlist(lw_smooth, pareto_k)
 }
