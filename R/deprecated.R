6cfc95f64c6853ff8006481d67e18f7b86165803
jgabry <jgabry@gmail.com>
2015-07-16 01:39:39 -0400

    doc

--- R/deprecated.R
+++ R/deprecated.R
@@ -70,32 +70,10 @@ loo_and_waic <- function(log_lik, ...) {
 }
 
 
-#' Model comparison
-#'
-#' This function is deprecated. Please use \code{\link{compare}} instead.
-#'
+
+#' @rdname loo_and_waic
 #' @keywords internal
 #' @export
-#'
-#' @param loo1,loo2 lists (of class \code{'loo'}).
-#' @return a named list with class \code{'compare.loo'}.
-#'
-#' @details When comparing two fitted models, we can estimate the difference in
-#'   their expected predictive accuracy by the difference in \code{elpd_waic} or
-#'   \code{elpd_loo} (multiplied by -2, if desired, to be on the deviance
-#'   scale). To compute the standard error of this difference we can use a
-#'   paired estimate to take advantage of the fact that the same set of \eqn{N}
-#'   data points is being used to fit both models. We would think that these
-#'   calculations would be most useful when \eqn{N} is large, because then
-#'   non-normality of the distribution is not such an issue when estimating the
-#'   uncertainty of these sums. In any case, we suspect that these standard
-#'   errors, for all their flaws, should give a better sense of uncertainty than
-#'   what is obtained using the current standard approach of comparing
-#'   differences of deviances to a Chi-squared distribution, a practice derived
-#'   for Gaussian linear models or asymptotically and which only applies to
-#'   nested models in any case.
-#'
-#'
 loo_and_waic_diff <- function(loo1, loo2) {
 
   .Deprecated("compare()")
