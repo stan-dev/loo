69ecc1def56b773eebefae6432e7b69a53f84190
jgabry <jgabry@gmail.com>
2017-08-16 15:54:28 -0400

    Weakly informative prior on k
    [ci skip]

--- R/gpdfit.R
+++ R/gpdfit.R
@@ -1,7 +1,10 @@
 #' Generalized Pareto distribution
 #'
 #' Estimate the parameters \eqn{k} and \eqn{\sigma} of the generalized Pareto
-#' distribution, given a sample \eqn{x}.
+#' distribution, given a sample \eqn{x}. The Pareto fit uses a prior for
+#' \eqn{k}, which will stabilize estimates for very small Monte Carlo sample
+#' sizes and low effective sample sizes. The weakly informative prior is a
+#' Gaussian prior centered on 0.5.
 #'
 #' @keywords internal
 #' @export
@@ -13,7 +16,7 @@
 #'
 #' @template internal-function-note
 #'
-#' @seealso \code{\link{psislw}}, \code{\link{pareto-k-diagnostic}},
+#' @seealso \code{\link{psis}}, \code{\link{pareto-k-diagnostic}},
 #'   \code{\link{loo-package}}
 #'
 #' @references
@@ -35,13 +38,26 @@ gpdfit <- function(x) {
   bdotw <- sum(b * w)
   k <- mean.default(log1p(-bdotw * x))
   sigma <- -k / bdotw
+  k <- adjust_k(k, n = N)
   nlist(k, sigma)
 }
 
 
 # internal ----------------------------------------------------------------
+
 lx <- function(a,x) {
   a <- -a
   k <- sapply(a, FUN = function(y) mean(log1p(y * x)))
   log(a / k) - k - 1
+}
+
+# Adjust k based on weakly informative Gaussian prior centered on 0.5. This will
+# stabilize estimates for very small Monte Carlo sample sizes and low neff
+# cases.
+# @param k khat estimate
+# @param n number of tail samples used to fit GPD
+adjust_k <- function(k, n) {
+  a <- 10
+  nplusa <- n + a
+  k * n / nplusa + a * 0.5 / nplusa
 }
--- tests/testthat/test_gpdfit.R
+++ tests/testthat/test_gpdfit.R
@@ -5,7 +5,5 @@ test_that("gpdfit returns correct result", {
   set.seed(123)
   x <- rexp(100)
   gpdfit_val <- unlist(gpdfit(x))
-  gpdfit_ans <- structure(c(0.0274030348712631, 1.01829821712701),
-                          .Names = c("k", "sigma"))
-  expect_equal(gpdfit_val, gpdfit_ans)
+  expect_equal_to_reference(gpdfit_val, "gpdfit.rds")
 })
