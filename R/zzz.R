9c6f602672dc7fbadc0769faae969ce3cd1eeee2
jgabry <jgabry@gmail.com>
2017-11-17 16:20:25 -0500

    Mention change to default number of cores in startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,10 @@
 .onAttach <- function(...) {
   ver <- utils::packageVersion("loo")
-  packageStartupMessage("This is loo version ", ver)
+  packageStartupMessage(
+    "This is loo version ", ver, ". ",
+    "NOTE: as of version 2.0.0 loo defaults to 1 core ",
+    "but we recommend using as many as possible. ",
+    "Use the 'cores' argument or set options(loo.cores = VALUE) ",
+    "for an entire session."
+  )
 }
