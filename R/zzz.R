137a5e37910542bdc8f0d7a82689b5fb1e2c82cc
jgabry <jgabry@gmail.com>
2018-03-30 14:03:24 -0400

    Update zzz
    [ci skip]

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(...) {
   ver <- utils::packageVersion("loo")
   packageStartupMessage(
-    "This is loo version ", ver, ".\n",
+    "This is loo version ", ver, " (see package NEWS for details on major changes).\n",
     "NOTE: as of version 2.0.0 loo defaults to 1 core ",
     "but we recommend using as many as possible.\n",
     "Use the 'cores' argument or set options(mc.cores = NUM_CORES) ",
