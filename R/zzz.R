6698455444a73709075878b568f0e4b06fe6d8fe
Aki Vehtari <Aki.Vehtari@aalto.fi>
2019-02-03 11:41:38 +0200

    add warning about Windows 10 core problem #94

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,8 @@
     "**NOTE: As of version 2.0.0 loo defaults to 1 core ",
     "but we recommend using as many as possible. ",
     "Use the 'cores' argument or set options(mc.cores = NUM_CORES) for an entire session. ",
+    "Windows 10 can be super slow if \code{mc.cores} is set in \code{.rprofile}",
+    "(see https://github.com/stan-dev/loo/issues/94)."
     "Visit mc-stan.org/loo/news for details on other changes."
   )
 }
