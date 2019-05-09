ec543026178bceb8a4a51ff88762fc6228ec0956
jgabry <jgabry@gmail.com>
2019-05-09 19:39:35 -0400

    add more doc for kfold generic

--- R/kfold-generic.R
+++ R/kfold-generic.R
@@ -1,9 +1,14 @@
 #' Generic function for K-fold cross-validation for developers
 #'
-#' For developers of modeling packages, \pkg{loo} includes a generic function
-#' \code{kfold} so that methods may be defined for K-fold CV without name
-#' conflicts between packages. See, e.g., the \code{kfold.stanreg} method in
-#' \pkg{rstanarm} and the \code{kfold.brmsfit} method in \pkg{brms}.
+#' @description For developers of Bayesian modeling packages, \pkg{loo} includes
+#'   a generic function \code{kfold} so that methods may be defined for K-fold
+#'   CV without name conflicts between packages. See, for example, the
+#'   \code{kfold} methods in \pkg{rstanarm} and \pkg{brms}.
+#'
+#'   The \strong{Value} section below describes the objects that \code{kfold}
+#'   methods should return in order to be compatible with
+#'   \code{\link{loo_compare}} and the \pkg{loo} package print methods.
+#'
 #'
 #' @name kfold-generic
 #' @param x A fitted model object.
@@ -18,6 +23,9 @@
 #'    \item \code{"pointwise"}: an Nx1 matrix with column name "elpd_kfold" containing
 #'    the pointwise contributions for each data point.
 #'   }
+#'   It is important for the object to have at least these classes and
+#'   components so that it is compatible with other functions like
+#'   \code{\link{loo_compare}} and print methods.
 #'
 NULL
 
