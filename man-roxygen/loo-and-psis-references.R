674d74678313fd7b3694419e31fb3d6ffc770f4c
jgabry <jgabry@gmail.com>
2020-12-01 13:39:42 -0700

    fix CRAN complaints about arxiv urls

--- man-roxygen/loo-and-psis-references.R
+++ man-roxygen/loo-and-psis-references.R
@@ -7,5 +7,5 @@
 #'
 #' Vehtari, A., Simpson, D., Gelman, A., Yao, Y., and Gabry, J. (2019).
 #' Pareto smoothed importance sampling.
-#' [preprint arXiv:1507.02646](https://arxiv.org/abs/1507.02646/)
+#' [preprint arXiv:1507.02646](https://arxiv.org/abs/1507.02646)
 #'
