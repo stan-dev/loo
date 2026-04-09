faaca770161481df5c3de518c7a1f0a2320edcdb
VisruthSK <public@visruth.com>
2026-04-08 18:14:58 -0700

    Generated tests

--- touchstone/script.R
+++ touchstone/script.R
@@ -1,30 +1,73 @@
 # see `help(run_script, package = 'touchstone')` on how to run this
 # interactively
 
-# TODO OPTIONAL Add directories you want to be available in this file or during the
-# benchmarks.
-# touchstone::pin_assets("some/dir")
-
 # installs branches to benchmark
 touchstone::branch_install()
 
-# benchmark a function call from your package (two calls per branch)
+# These synthetic workloads are large enough to expose real slowdowns in the
+# core `loo()` paths, but still short enough to keep PR feedback reasonably fast.
 touchstone::benchmark_run(
-  # expr_before_benchmark = source("dir/data.R"), #<-- TODO OTPIONAL setup before benchmark
-  random_test = yourpkg::f(), #<- TODO put the call you want to benchmark here
-  n = 2
+  expr_before_benchmark = {
+    suppressPackageStartupMessages(library(loo))
+
+    matrix_draws <- 2000L
+    matrix_obs <- 500L
+    n_chains <- 4L
+    stopifnot(matrix_draws %% n_chains == 0L)
+
+    set.seed(20260408)
+    mu <- stats::rnorm(matrix_draws)
+    sigma <- exp(stats::rnorm(matrix_draws, mean = -0.2, sd = 0.15))
+    y <- stats::rnorm(matrix_obs, mean = 0.3, sd = 1.2)
+
+    log_lik_matrix <- vapply(
+      y,
+      FUN = function(y_i) {
+        stats::dnorm(y_i, mean = mu, sd = sigma, log = TRUE)
+      },
+      FUN.VALUE = numeric(matrix_draws)
+    )
+    matrix_r_eff <- rep(1, matrix_obs)
+
+    n_iter <- matrix_draws / n_chains
+    log_lik_array <- array(NA_real_, dim = c(n_iter, n_chains, matrix_obs))
+    for (chain in seq_len(n_chains)) {
+      rows <- ((chain - 1L) * n_iter + 1L):(chain * n_iter)
+      log_lik_array[, chain, ] <- log_lik_matrix[rows, ]
+    }
+
+    function_obs <- 250L
+    function_data <- data.frame(y = y[seq_len(function_obs)])
+    function_draws <- cbind(mu = mu, sigma = sigma)
+    function_r_eff <- rep(1, function_obs)
+    llfun <- function(data_i, draws) {
+      stats::dnorm(
+        data_i$y,
+        mean = draws[, "mu"],
+        sd = draws[, "sigma"],
+        log = TRUE
+      )
+    }
+  },
+  loo_matrix = {
+    suppressWarnings(loo(log_lik_matrix, r_eff = matrix_r_eff, cores = 1))
+  },
+  loo_array = {
+    suppressWarnings(loo(log_lik_array, r_eff = matrix_r_eff, cores = 1))
+  },
+  loo_function = {
+    suppressWarnings(
+      loo(
+        llfun,
+        data = function_data,
+        draws = function_draws,
+        r_eff = function_r_eff,
+        cores = 1
+      )
+    )
+  },
+  n = 10
 )
-
-# TODO OPTIONAL benchmark any R expression (six calls per branch)
-# touchstone::benchmark_run(
-#   more = {
-#     if (TRUE) {
-#       y <- yourpkg::f2(x = 3)
-#     }
-#   }, #<- TODO put the call you want to benchmark here
-#   n = 6
-# )
-
 
 # create artifacts used downstream in the GitHub Action
 touchstone::benchmark_analyze()
