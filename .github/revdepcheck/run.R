f8b2c8efe226bf1320f25cee5260bbaaec45f26d
VisruthSK <public@visruth.com>
2026-07-15 14:50:55 -0700

    Trying to fix lib paths?

--- .github/revdepcheck/run.R
+++ .github/revdepcheck/run.R
@@ -9,5 +9,11 @@ revdepcheck::revdep_check(
   timeout = as.difftime(
     as.integer(Sys.getenv("TIMEOUT_MINUTES")),
     units = "mins"
+  ),
+  quiet = FALSE,
+  env = c(
+    revdepcheck::revdep_env_vars(),
+    R_LIBS_SITE = normalizePath(.libPaths()[1], mustWork = TRUE)
   )
 )
+
